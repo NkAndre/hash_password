56fc5b8d8eaa037cdd3f527622045d15edfd570d
NkAndre <andre270708@gmail.com>
2026-07-28 22:10:45 -0300

    verificao de senha(funcional

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ senha_criptografada =bcrypt.hashpw(senha_bytes,salt)
 
 #print(f'Hash Gerado : {senha.decode('utf-8')}')
 print(f"Hash gerado: {senha_criptografada.decode('utf-8')}")
+
+print('\n--- Simulção de Login --- ')
+
+senha_teste = input('Digite sua senha novamente para logar:').encode('utf-8')
+
+if bcrypt.checkpw(senha_teste,senha_criptografada):
+    print('Sucesso : Senha Correta!')
+else:
+    print('Erro : Senha Incorreta')
