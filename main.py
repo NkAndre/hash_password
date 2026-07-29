603dfc034493d832239ed6226f33dad23c5b82dc
NkAndre <andre270708@gmail.com>
2026-07-28 22:06:23 -0300

    feat:logica de hash ta funcionando

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import bcrypt
+
+senha_texto = (input('Digite sua senha : '))
+senha_bytes = senha_texto.encode('utf-8')
+
+salt = bcrypt.gensalt()
+senha_criptografada =bcrypt.hashpw(senha_bytes,salt)
+
+#print(f'Hash Gerado : {senha.decode('utf-8')}')
+print(f"Hash gerado: {senha_criptografada.decode('utf-8')}")
